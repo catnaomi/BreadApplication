f481b6530ab5050a55378adae58f3a061ec353c0
cat <cat.monden@gmail.com>
2019-02-17 15:37:43 -0500

    ui work + firebase progress
    thx rohith

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import * as firebase from 'firebase';
 import {
   StyleSheet,
   Text,
@@ -14,9 +15,10 @@ export default class BreadApp extends Component {
     return (
       <View style = {{top: '0%', height: '100%'}}>
         <Image
-          source={require('./assets/images/logos/basetext.png')}
-          style= {{flex: 2, width: '50%', height: '40%', top: '10%', left: '25%'}}
+          source={require('./assets/images/logos/texthoriz.png')}
+          style= {{position: 'absolute', width: '100%', top: 0}}
         />
+        <View style = {{flex : 1}}></View>
         <View style = {{flex: 1, backgroundColor: 'powderblue'}}>
           <TextInput
             style={styles.loginField}
@@ -28,26 +30,45 @@ export default class BreadApp extends Component {
             placeholder = "password"
             onChangeText = {(text) => this.setState({text})}
           ></TextInput>
-          <Button
-            style = {{flex: 1}}
-            onPress={() => {
-              Alert.alert('You tapped the button!');
-            }}
-            title="Press Me"
-          />
+          <View style = {styles.loginButton}>
+            <Button
+              onPress={() => {
+                Alert.alert('user is in database!');
+              }}
+              title="Login"
+            />
+          </View>
         </View>
-        <View style = {{flex: 2, backgroundColor: 'black'}}></View>
+        <View style = {{flex: 2, backgroundColor: 'lightgrey'}}>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create ({  
   loginField: {
     left: '10%',
     width: '80%',
     borderBottomWidth: 1,
-    flex: 1,
-    margin: 10
+    margin: 10,
+    flex: 1
+  },
+  loginButton: {
+    left: '30%',
+    width: '40%',
+    margin: 10,
+    flex: 1
   }
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoG-0V4r7eJsauSsOhfU-Gx0cduMTfQZc",
+  authDomain: "juniordesign-afa7c.firebaseapp.com",
+  databaseURL: "https://juniordesign-afa7c.firebaseio.com",
+  projectId: "juniordesign-afa7c",
+  storageBucket: "juniordesign-afa7c.appspot.com",
+  messagingSenderId: "199705549356"
+};
+
+firebase.initializeApp(firebaseConfig);
