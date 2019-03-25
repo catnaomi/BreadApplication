f0367ca39aa0c91ec02793e0410bb807b814700f
cat <cat.monden@gmail.com>
2019-03-25 14:41:42 -0400

    refactoring for merge

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import LandingScreen from "./screens/LandingScreen"
 import UserScreen from "./screens/UserScreen"
 import LoginScreen from "./screens/LoginScreen"
 import BusinessPage from "./screens/BusinessPage"
+import SearchScreen from "./screens/SearchScreen"
+
 import {
   StyleSheet,
   Text,
@@ -32,34 +34,6 @@ export class FavoriteScreen extends Component {
   }
 }
 
-export class SearchScreen extends Component {
-
-  constructor (props) {
-    super(props);
-  }
-
-  render() {
-        var profile = require('./assets/images/profile/julia.png');
-        return (
-            <View style = {{width: '100%', height: '100%'}}>
-                {/*Profile header*/}
-                <View style = {styles.searchHeader}>
-                    <View style = {{flex : 1, borderLeftWidth: 1, justifyContent: 'center', alignContent: 'center'}}>
-                        <Text style = {{textAlign: 'center', fontSize: 18}}>
-                            Businesses Found: 
-                        </Text>
-                    </View>
-                </View>
-                <View style = {styles.searchContent}>
-                    <View style = {{left: 10, top: 10}}>
-                        <Text>Searching Businesses from Database needs to be implemented</Text>
-                    </View>
-                </View>
-            </View>
-        );
-    }
-}
-
 
 export class SettingsScreen extends Component {
     render() {
@@ -82,25 +56,3 @@ const TabNavigator = createBottomTabNavigator({
 });
 
 export default createAppContainer(TabNavigator);
-
-const styles = StyleSheet.create ({
-    searchBar: {
-    position: 'absolute',
-    top: 50,
-    left: '10%',
-    height: 50,
-    width: '80%',
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 10,
-    },
-    searchHeader: {
-        flex: 3,
-        borderWidth: 1,
-        backgroundColor: 'lightgrey',
-    },
-    searchContent: {
-      flex: 6,
-      borderWidth: 1,
-    },
-});
