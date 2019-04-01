90460eb3f5586d06520c1fcc7b9d013760c3fdec
cat <cat.monden@gmail.com>
2019-03-31 20:50:11 -0400

    database fetching for businesses

--- screens/Review.js
+++ screens/Review.js
@@ -2,9 +2,20 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from "react-native";
 
 export default class Review extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            id: this.props.id,
+            content: '',
+            user_id: '',
+            business_id: '',
+            date: 0,
+        };
+    }
     render () {
         return (
             <View style = {styles.Review}>
+                <View></View>
                 <Text>Author: {this.props.name}</Text>
                 <Text>Business: {this.props.business}</Text>
                 <Text>Rating: {this.props.rating}</Text>
@@ -23,4 +34,11 @@ const styles = StyleSheet.create ({
         borderBottomWidth: 1,
         borderColor: 'grey',
     },
+    ReviewHeader: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    ReviewProfile: {
+
+    }
 });
