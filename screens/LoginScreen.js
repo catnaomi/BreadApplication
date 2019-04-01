d50c737ea42005190ba7822b03b1ae74aa04015f
My Git Stuff <rmarsel3@gatech.edu>
2019-03-31 22:34:35 -0400

    Implemented login verification

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Alert, Button, StyleSheet, Text, TextInput, TouchableHighlight, View, Image} from "react-native";
 import {navigate} from 'react-navigation';
 import RegisterScreen from './RegisterScreen';
-//import {registerUser} from "../db/firebase";
+import {getUserData, doesUserExist} from "../db/firebase";
 import { createStackNavigator } from 'react-navigation';
 
 class LoginScreen extends Component {
@@ -16,7 +16,7 @@ class LoginScreen extends Component {
     render() {
         var logo = require('../assets/images/logos/texthoriz.png');
         const { navigate } = LoginStack;
-
+        var sel = this;
         return (
             <View style = {{flex: 1}}>
                 <View style = {styles.imageView}>
@@ -40,8 +40,35 @@ class LoginScreen extends Component {
                     <View style = {styles.loginButton}>
                         <Button
                             onPress={() => {
-                                Alert.alert(this.state.username + '\nadded to database');
-                                registerUser(this.state.username, this.state.password);
+                                if (sel.state.username == '' || sel.state.password == '') {
+                                    Alert.alert("One or more fields missing input!");
+                                } else if (!validate(sel.state.username)) {
+                                    Alert.alert("E-mail address is invalid!");
+                                } else if (sel.state.password.length < 7) {
+                                    Alert.alert("Password is not long enough!");
+                                } else {
+                                //console.log(typeof(doesUserExist(sel.state.username)));
+                                    doesUserExist(sel.state.username).then(response => {
+                                        if (response) {
+                                            getUserData(sel.state.username).then(response => {
+                                                if (sel.state.password == response.hash_pass) {
+                                                    console.log("Login successful! Logging in");
+                                                    Alert.alert('Logging in\n' + response.name);
+                                                    //Do login stuff
+                                                } else {
+                                                    console.log("Login failed! Incorrect password.");
+                                                    Alert.alert('Username or password is incorrect');
+                                                    //Keep track of failed logins, maybe
+                                                }
+                                            })
+                                        } else {
+                                            console.log("Login failed! No such user exists.");
+                                            Alert.alert(sel.state.username + 'User does not exist');
+                                        }
+
+                                    })
+                                }
+                                //Alert.alert(this.state.username + '\nadded to database');
                             }}
                             title="Login"
                         />
@@ -131,3 +158,9 @@ export const LoginStack = createStackNavigator({
         headerVisible: false,
     }
 });
+
+const validate = (email) => {
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+    return expression.test(String(email).toLowerCase())
+}
